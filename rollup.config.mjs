c24c03d600d275d504c40e3c17c09bc1fb1630c6
364864264 <364864264@qqcom>
2023-12-24 13:50:38 +0800

    提交

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,34 +2,32 @@
 import { terser } from 'rollup-plugin-terser'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import vue from 'rollup-plugin-vue'
-import css from 'rollup-plugin-css-only'
-import scss from 'rollup-plugin-scss'
 import babel from 'rollup-plugin-babel'
 import typescript from 'rollup-plugin-typescript2'
 import commonjs from '@rollup/plugin-commonjs'
 import replace from 'rollup-plugin-replace'
 import postcss from 'rollup-plugin-postcss'
+import discardComments from 'postcss-discard-comments'
+import nodePolyfills from 'rollup-plugin-polyfill-node'
 import json from '@rollup/plugin-json'
 import autoprefixer from 'autoprefixer'
 import cssnano from 'cssnano'
-import CleanCSS from 'clean-css' // 压缩css
-import { writeFileSync } from 'fs' // 写文件
 
 export default {
   input: 'src/index.ts',
-  external: ['vue', 'lodash-es', 'lodash'],
+  external: ['vue', 'lodash'],
   output: [
     {
       file: 'lib/index.esm.js',
       format: 'esm'
     },
     {
-      file: 'lib/xl-ui.umd.js',
+      file: 'lib/index.umd.js',
       format: 'umd',
       name: 'xl-ui2',
       globals: {
         vue: 'Vue',
-        'lodash-es': 'lodashEs'
+        lodash: 'lodash'
       }
     }
   ],
@@ -43,7 +41,8 @@ export default {
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
     typescript(),
-    vue(),
+    vue({}),
+    nodePolyfills(),
     babel({
       extensions: ['.vue', '.ts', '.js', '.tsx', '.jsx'],
       exclude: 'node_modules/**',
@@ -51,19 +50,13 @@ export default {
       //plugins: ['external-helpers'],
       //externalHelpers: true
     }),
-    scss(),
     //terser(),
     postcss({
-      //plugins: [autoprefixer(), cssnano()],
-      extract: '../dist/index.css'
+      plugins: [autoprefixer(), discardComments(), cssnano()],
+      extract: 'index.css',
+      extensions: ['.css']
     }),
     json()
-    /* css({
-      output(style) {
-        // 压缩 css 写入 dist/base-ui.css
-        //writeFileSync('dist/xl-ui.css', new CleanCSS().minify(style).styles)
-      }
-    }) */
   ],
   // 用来指定代码执行环境的参数，解决this执行undefined问题
   context: 'window'
