5e20dac4e8637aeb3836b920f9a1fc71ae668e30
364864264 <364864264@qqcom>
2024-01-26 18:07:49 +0800

    新增组件

---
+++ src/views/BaseForm.vue
@@ -0,0 +1,118 @@
+<template>
+  <div>
+    <XlForm
+      ref="myform"
+      :form-data="formData"
+      :form-item="formItem"
+      :rules="rules"
+      :form-attribute="formAttribute"
+      @change="handleSaveForm"
+    ></XlForm>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'BaseForm',
+  components: {}
+})
+export default class extends Vue {
+  formData = {
+    org: '111111',
+    jddw: '0592',
+    ifjt: '1',
+    source: ['2', '3'],
+    kpr: '1-2',
+    address: ['350000', '350200', '350211'],
+    gdsj: '2020-12-20 08:00:20',
+    jdsj: '',
+    fdsj: ['2020-12-20 08:00:20', '2024-01-25 00:00:00'],
+    isgd: '1'
+  }
+  formItem = [
+    {
+      label: '工单编号',
+      prop: 'org',
+      formItemAttr: {},
+      attribute: {}
+    },
+    {
+      label: '接单单位',
+      prop: 'jddw',
+      type: 'select',
+      formItemAttr: {},
+      attribute: { labelname: 'jddwName', code: '1' }
+    },
+    {
+      label: '是否接通',
+      prop: 'ifjt',
+      type: 'radio',
+      formItemAttr: {},
+      attribute: { labelname: 'sfjtName', code: '4' }
+    },
+    {
+      label: '来源',
+      prop: 'source',
+      type: 'checkbox',
+      formItemAttr: {},
+      attribute: { labelname: 'sourceName', code: '9' }
+    },
+    {
+      label: '开票人',
+      prop: 'kpr',
+      type: 'tree',
+      formItemAttr: {},
+      attribute: { labelname: 'kprName' }
+    },
+    {
+      label: '故障地址',
+      prop: 'address',
+      type: 'cascader',
+      formItemAttr: {},
+      attribute: { labelname: 'addressName' }
+    },
+    {
+      label: '故障时间',
+      prop: 'gdsj',
+      type: 'date',
+      formItemAttr: {},
+      attribute: {}
+    },
+    {
+      label: '接单时间',
+      prop: 'jdsj',
+      type: 'datetime',
+      formItemAttr: {},
+      attribute: {}
+    },
+    {
+      label: '复电时间',
+      prop: 'fdsj',
+      type: 'date2',
+      span: 24,
+      formItemAttr: {},
+      attribute: {}
+    },
+    {
+      label: '是否归档',
+      prop: 'isgd',
+      type: 'switch',
+      formItemAttr: {},
+      attribute: {}
+    }
+  ]
+  formAttribute = {}
+  rules = {
+    org: [{ required: true, message: '请输入工单编号' }],
+    jddw: [{ required: true, message: '请选择接单单位' }]
+  }
+
+  public handleSaveForm(data: object): void {
+    console.log(data)
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ src/views/BaseTable.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <XlTable
+      :sourceData="sourceData"
+      :columns="columns"
+      :total="total"
+      :currentPage.sync="currentPage"
+      :pageSize.sync="pageSize"
+      :page-sizes="pageSizes"
+      :table-attribute="{ 'max-height': 500 }"
+      selection
+      @change="handleChangePage"
+      @selection-change="handleSelect"
+    ></XlTable>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'MyXlTable',
+  components: {}
+})
+export default class extends Vue {
+  sourceData = []
+  columns = [
+    {
+      label: '序号',
+      prop: 'idx'
+    }
+  ]
+  currentPage = 1
+  pageSize = 10
+  total = 0
+}
+</script>
+
+<style scoped></style>
